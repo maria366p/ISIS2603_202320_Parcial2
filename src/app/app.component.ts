a433cd1b51ea3ef47551f4b30390f402f69475df
Maria Ospina <mp.ospinap1@uniandes.edu.co>
2023-11-28 00:34:10 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ISIS2603_202320_Parcial2';
+}
